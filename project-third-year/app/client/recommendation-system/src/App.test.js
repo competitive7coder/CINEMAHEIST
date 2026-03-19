5c6d7567144cbd87ab08dbc8177d4b5670f6bad2
Protyush <ghorui.protyushraj@gmail.com>
2026-03-19 17:07:56 +0530

    Removed app/client folder

--- project-third-year/app/client/recommendation-system/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders streamhub branding on initial load', () => {
-  render(<App />);
-  const brandingElements = screen.getAllByText(/streamhub/i);
-  expect(brandingElements.length).toBeGreaterThan(0);
-});

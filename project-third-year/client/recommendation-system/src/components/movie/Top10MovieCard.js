b87fb3f7053a6b8560c07a931e8595f48402aaea
Protyush <ghorui.protyushraj@gmail.com>
2026-04-03 12:51:39 +0530

    fix: correct srcset sizes to match 154px card width

--- project-third-year/client/recommendation-system/src/components/movie/Top10MovieCard.js
+++ project-third-year/client/recommendation-system/src/components/movie/Top10MovieCard.js
@@ -191,7 +191,7 @@ const Top10MovieCard = ({
     https://image.tmdb.org/t/p/w185${movie.poster_path} 185w,
     https://image.tmdb.org/t/p/w342${movie.poster_path} 342w
   `}
-          sizes="200px"
+          sizes="154px"
           alt={movie.title}
           className="t10-poster"
           loading="lazy"

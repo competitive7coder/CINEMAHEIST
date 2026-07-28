d4e08d1f0858ccf1250fb280a67b44fb8598e723
Protyush <ghorui.protyushraj@gmail.com>
2026-07-28 15:02:32 +0530

    feat(pwa): link manifest.json and register service-worker to make site installable

---
+++ project-third-year/client/recommendation-system/public/service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "streamhub-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/logo192.png",
+  "/logo512.png"
+];
+
+// Install Service Worker and cache resources
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate Service Worker and clean up old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch events
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
